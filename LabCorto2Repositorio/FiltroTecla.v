3e1a3e913090fc13340e96623158ec9070ad32a0
manchii <kaleb.23415@gmail.com>
2015-03-26 11:01:17 -0600

    Comentarios

--- LabCorto2Repositorio/FiltroTecla.v
+++ LabCorto2Repositorio/FiltroTecla.v
@@ -10,7 +10,7 @@
 // Target Devices: 
 // Tool versions: 
 // Description: 
-//
+//	Módulo encargado para habilitar la lectura después del código de ruptura F0 del protocolo del teclado
 // Dependencies: 
 //
 // Revision: 
@@ -20,24 +20,27 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 module FiltroTecla(
-	//Dato entrante del teclado
+	//Dato entrante capturado del teclado
 	input wire [7:0] Dato_rx,
-	//Señal de nuevo dato entrante
+	//Señal de dato entrante listo para leerse
 	input wire rx_done_tick,
-	//Señal clk y rst
+	//Señal clock y reset
 	input wire clk,rst,
 	//Señal de permiso a leer
 	output reg filtro_enable
     );
-//se declaran los estados y sus valores iniciales
+//se declaran los estados y sus valores de manera simbólica
 localparam [1:0]
 	idle = 2'h0,
 	f0 = 2'h1,
 	espera = 2'h2,
 	leer = 2'h3;
 
+//Registro para alojar los estados
 	reg [1:0] filtro_reg, filtro_sig;
 	
+//Descripción de la máquina de estados
+//Inicio
 	always@(posedge clk, posedge rst)
 	begin
 		if(rst)
@@ -46,23 +49,25 @@ localparam [1:0]
 			filtro_reg <= filtro_sig;
 	end
 
+//Descripción de las transiciones y las salidas
 	always@*
 	begin
 		filtro_sig = filtro_reg;
 		filtro_enable = 1'b0;
 		case(filtro_reg)
 			idle:
-				if(rx_done_tick)//espera a que el teclado envie el mensaje de mensaje enviado para empezar la maquina
+			//espera a que el módulo de recepción de datos habilite la lectura para empezar la transición
+				if(rx_done_tick)
 					filtro_sig = f0;
 			f0:
 				if(Dato_rx==8'hf0)//revisa si el dato que ingresa corresponde a un f0 en hexadecimal 
-					filtro_sig = espera;//si se cumple la condicion se avanza al estado de espera
+					filtro_sig = espera;//si se cumple la condición se avanza al estado de espera
 				else
 					filtro_sig = idle;//si no se cumple se regresa al estado de idle
 			espera:
-				if(rx_done_tick)//espera que llegue de nuevo el aviso desde el teclado que se envio un codigo de tecla
+				if(rx_done_tick)//Se espera hasta llegue la señal de habilitación de lectura
 				begin
-					//de darse la condicion se avanza al estado de leer  el dato de tecla que entro
+					//de darse la condicion se avanza al estado de leer
 					//luego envia un bit en alto para el enable
 					filtro_sig = leer;
 					filtro_enable = 1'b1;
@@ -75,5 +80,7 @@ localparam [1:0]
 			end	
 		endcase
 	end
+//Fin
+
 
 endmodule
